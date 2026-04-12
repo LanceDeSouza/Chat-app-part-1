adb81504152e4b09680ee65f9e34b11dbdab079d
LanceDeSouza <ST10499391@rcconnect.edu.za>
2026-04-12 18:19:04 +0200

    Initial project structure and Login class

---
+++ Login.java
@@ -0,0 +1,33 @@
+import java.util.regex.Pattern;
+
+/*================Login class================
+ * Registration and login feature
+ * 
+ * This class is responsible for handling the users data and for validating their information by checking
+ * User name (contains an underscore and has a max of 5 characters)
+ * Password (has a minimum of 8 chars, a capital letter, a number and a special character)
+ * Cell phone number (South African format: +27 and followed by 9 other digits)
+*/
+public class Login {
+		
+//Declaring the private variables
+		private String userName;
+		private String password;
+		private String cellPhoneNum;
+		private String firstName;
+		private String lastName;
+
+//Empty constructor
+		public Login() {
+			}
+
+//Constructor with parameters
+	public Login (String userName, String password, String cellPhoneNum, String firstName, String lastName) {
+		
+		this.userName = userName;
+		this.password = password;
+		this.cellPhoneNum = cellPhoneNum;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+}
